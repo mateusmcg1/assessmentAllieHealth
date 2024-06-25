61410819383cb115cee11f792eca7e0eee0546ca
mateusmcg1 <mateusmartinscg@gmail.com>
2024-06-25 00:02:20 -0300

    second challenge

--- server/migrations/2.sql
+++ server/migrations/2.sql
@@ -1,7 +1,7 @@
-INSERT INTO users (first_name, last_name, email) VALUES
-    ('John', 'Doe', 'john.doe@alliehealth.com'),
-    ('Ethan', 'Turner', 'ethan.turner@alliehealth.com'),
-    ('Olivia', 'Martinez', 'olivia.m@alliehealth.com'),
-    ('Liam', 'Anderson', 'liam.a@alliehealth.com'),
-    ('Sophia', 'Reynolds', 'sophia.r@alliehealth.com'),
-    ('Mary', 'Jane', 'mary.jane@alliehealth.com');
+INSERT INTO users (first_name, last_name, email, birthdate) VALUES
+    ('John', 'Doe', 'john.doe@alliehealth.com', '1990-04-20'),
+    ('Ethan', 'Turner', 'ethan.turner@alliehealth.com', '1990-04-21'),
+    ('Olivia', 'Martinez', 'olivia.m@alliehealth.com', '1990-04-22'),
+    ('Liam', 'Anderson', 'liam.a@alliehealth.com', '1990-04-23'),
+    ('Sophia', 'Reynolds', 'sophia.r@alliehealth.com', '1990-04-24'),
+    ('Mary', 'Jane', 'mary.jane@alliehealth.com', '1990-04-25');
